a052c140719719db1b8bf700f8fec68a7c22491a
Tomekxm <tomekxm@onet.pl>
2022-03-23 21:15:06 +0100

    Czyszczenie komentarzy

--- Zad2/Program.cs
+++ Zad2/Program.cs
@@ -11,18 +11,3 @@ namespace Zad2
         }
     }
 }
-
-/* 
-  Napisz program sprawdzający poprawność działania matematycznego,
-  w którym usunięto wszystkie liczby i operatory a pozostawiono jedynie 
-  nawiasy okrągłe, np. (()()) lub )()), przy czy pierwszy przykład jest zapisem prawidłowym a drugi nieprawidłowym:
-    • Program powinien zwrócić flagę typu Bool, przy czym:
-        ◦ wartość true oznacza zapis prawidłowy
-        ◦ wartość false zapis nieprawdiłowy 
-    • Program powinien przyjmować jeden argument typu String. Wartość tego parametru to dowolny ciąg znaków ‘(‘ oraz ‘)’.
-    • Napisać test jednostkowy, który sprawdza działanie metody na reprezentatywnych przykładach, np. dla następujących wartości:
-        ◦ (((()()))())
-        ◦ (((()()))()))
-        ◦ )((((()))()))
-    • Proszę dodać kilka własnych wartości w w/w teście jednostkowym
-*/
